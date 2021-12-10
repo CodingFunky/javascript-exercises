fef7f586821c085331f29270321a4befd99dfaa2
BananaBJones <95904947+CodingFunky@users.noreply.github.com>
2021-12-10 00:56:18 -0700

    Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = '';
 
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            newString += string; 
+        }
+    } 
+    else {
+        return 'ERROR';
+    }
+
+    return newString;
 };
+
+console.log(repeatString('hey', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(Fn) {
+    let fib = [0, 1];
+    if (Fn >= 0) {
+        for (i=2;i<=Fn;i++) {
+            fib[i] = fib[i-2] + fib[i-1];
+        }
+    } else {
+        return 'OOPS'
+    }
+    return fib[Fn];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci(5));
